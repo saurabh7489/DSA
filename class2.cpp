79a22672335faf12aec5d033b8148e0fd16d69ed
saurabh7489 <saurabhpandey7489@gmail.com>
2025-11-16 23:34:16 +0530

    2

--- class2.cpp
+++ class2.cpp
@@ -1,7 +1,28 @@
+// #include<iostream>
+// using namespace std;
+// int main ()
+// {
+//     cout<<"hello world ";
+//     return 0;
+// }
+// print your name 
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     cout<<"saurabh\npandey \n";
+//     return 0;
+// }
+
+// sum of two number 
 #include<iostream>
 using namespace std;
-int main ()
-{
-    cout<<"hello world ";
+int main(){
+    int a,b;
+    cout<<"enter the both number \n";
+    cin>>a;
+    cout<<"enter the both number \n";
+    cin>>b;
+    cout<<"the sum of two number is : "<<(a+b);
     return 0;
 }
