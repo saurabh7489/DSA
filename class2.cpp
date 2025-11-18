eb419266e74bed72361cea934656bc5216fa16b3
saurabh7489 <saurabhpandey7489@gmail.com>
2025-11-18 23:52:15 +0530

    2

--- class2.cpp
+++ class2.cpp
@@ -136,16 +136,30 @@
 // } 
 // }
 // star formation
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n=4;
+    
+//     for(int i=0;i<=n;i++){
+//         for(int j=1;j<=i+1;j++){
+//             cout<<'*';
+//             // st++;
+//         }
+//         cout<<endl;
+//     }
+// }
+
 #include<iostream>
 using namespace std;
-int main(){
+int main()
+{
     int n=4;
-    
-    for(int i=0;i<=n;i++){
-        for(int j=1;j<=i+1;j++){
-            cout<<'*';
-            // st++;
+    for(char i=1;i<=n;i++){
+
+        for(char j=1;j<=i;j++){
+           cout<<'A'; 
         }
         cout<<endl;
     }
-}
+} // namespace name
