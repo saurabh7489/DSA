bb8e26c54c97a4f5737409076d773eb5d97ea90b
saurabh7489 <saurabhpandey7489@gmail.com>
2025-11-21 19:42:20 +0530

    2

--- class2.cpp
+++ class2.cpp
@@ -178,18 +178,36 @@
 //     }
 // }
 
-#include<iostream>
-using namespace std;
-int main(){
-    int n=4;
-    int sum=1;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++){
-            cout<<sum;
-            sum++;
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n=4;
+//     int sum=1;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){
+//             cout<<sum;
+//             sum++;
             
             
 
-        }cout<<endl;
-    }
-}
+//         }cout<<endl;
+//     }
+// }
+
+// 
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n=4;
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<i;j++){
+//          cout<<' ';   
+//         }
+//         for(int j=1;j<=n-i;j++){
+            
+//         cout<<i+1;}
+    
+//     cout<<endl;
+// }
+// }
