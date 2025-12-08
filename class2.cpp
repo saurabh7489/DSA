412799056ad97d05dc8d54441227bf0a144628fe
saurabh7489 <saurabhpandey7489@gmail.com>
2025-12-08 16:02:53 +0530

    2

--- class2.cpp
+++ class2.cpp
@@ -242,30 +242,49 @@
 //  *   *
 //   * *
 //    *
-#include<iostream>
-using namespace std;
-int main(){
-    int n=4;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n-i-1;j++){
-            cout<<' ';
-        }cout<<'*';
-        if(i!=0){
-            for(int j=1;j<=2*i-1;j++){
-                cout<<" ";
-            }cout<<"*";
-        }cout<<endl;
-    }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n=4;
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<n-i-1;j++){
+//             cout<<' ';
+//         }cout<<'*';
+//         if(i!=0){
+//             for(int j=1;j<=2*i-1;j++){
+//                 cout<<" ";
+//             }cout<<"*";
+//         }cout<<endl;
+//     }
 
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<i+1;j++){
-            cout<<' ';
-        }cout<<'*';
-        if(i!=n-2){
-            for(int j=0;j<2*(n-i)-5;j++){
-                cout<<" ";
-             }  cout<<"*";
-        }cout<<endl;
-    }
+//     for(int i=0;i<n-1;i++){
+//         for(int j=0;j<i+1;j++){
+//             cout<<' ';
+//         }cout<<'*';
+//         if(i!=n-2){
+//             for(int j=0;j<2*(n-i)-5;j++){
+//                 cout<<" ";
+//              }  cout<<"*";
+//         }cout<<endl;
+//     }
     
-    }//hello demo
+//     }//hello demo
+
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n =4;
+    
+//     for(int i=1;i<=n;i++){
+//         char s=65;
+//         for(int j=1;j<=n;j++){
+//             cout<<s;
+            
+           
+//             s=s+1;
+//         }
+        
+//         cout<<endl;
+//     }
+// }
