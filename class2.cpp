dc4bb16aceddae69205b9b211de19cea0873fb56
saurabh7489 <saurabhpandey7489@gmail.com>
2025-12-10 22:51:36 +0530

    2

--- class2.cpp
+++ class2.cpp
@@ -363,17 +363,17 @@
 // }
 
 
-#include<iostream>
-using namespace std;
-int main(){
-    int n=4;
-    for(int i=0;i<=n-1;i++){
-        for(int j=0;j<=i-1;j++){
-            cout<<" ";
-        }
-        for(int k=1;k<=n-i;k++){
-          cout<<i+1;  
-        }
-        // cout<<i;
-        cout<<endl;}
-    }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n=4;
+//     for(int i=0;i<=n-1;i++){
+//         for(int j=0;j<=i-1;j++){
+//             cout<<" ";
+//         }
+//         for(int k=1;k<=n-i;k++){
+//           cout<<i+1;  
+//         }
+//         // cout<<i;
+//         cout<<endl;}
+//     }
---
+++ class3.cpp
@@ -0,0 +1,24 @@
+// function
+// #include<iostream>
+// using namespace std;
+
+// int min(int a,int b){
+//     int s=a-b;
+//     cout<<s;
+// }
+// int main(){
+//     min(4,2);
+
+// }
+#include<iostream>
+using namespace std;
+int min(int a,int b){
+    if(a>b){
+        cout<<"b is minimun";}
+        else{
+            cout<<"a is minimum";}
+        }
+    int main(){
+        min(2,3);
+    }
+    
