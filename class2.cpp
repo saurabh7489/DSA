9ae054765852033eb0941e31fe22a4fea40ca9a4
saurabh7489 <saurabhpandey7489@gmail.com>
2025-12-10 12:06:53 +0530

    2

--- class2.cpp
+++ class2.cpp
@@ -349,8 +349,31 @@
 
 
 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n=4;
+//     int l=1;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){
+//             cout<<l;
+//            l++; 
+//         }cout<<endl;
+//     }
+// }
+
+
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"yash";
-} 
+    int n=4;
+    for(int i=0;i<=n-1;i++){
+        for(int j=0;j<=i-1;j++){
+            cout<<" ";
+        }
+        for(int k=1;k<=n-i;k++){
+          cout<<i+1;  
+        }
+        // cout<<i;
+        cout<<endl;}
+    }
