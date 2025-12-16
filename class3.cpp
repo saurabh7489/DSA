2c24021ad60cb5ae0b28c41351e9d9308dcba0ba
saurabh7489 <saurabhpandey7489@gmail.com>
2025-12-16 14:11:59 +0530

    2

--- class3.cpp
+++ class3.cpp
@@ -10,15 +10,30 @@
 //     min(4,2);
 
 // }
-#include<iostream>
-using namespace std;
-int min(int a,int b){
-    if(a>b){
-        cout<<"b is minimun";}
-        else{
-            cout<<"a is minimum";}
-        }
-    int main(){
-        min(2,3);
-    }
+// #include<iostream>
+// using namespace std;
+// int min(int a,int b){
+//     if(a>b){
+//         cout<<"b is minimun";}
+//         else{
+//             cout<<"a is minimum";}
+//         }
+//     int main(){
+//         min(2,3);
+//     }
     
+// #include<iostream>
+// using namespace std;
+// int sum(int n){
+//     int s=0;
+//   for(int i=1;i<=n;i++){
+//     // int s=0;
+//     s=i+s;
+  
+  
+//     }  cout<<s;
+// }
+
+//   int main(){
+//     sum(4);
+//   }
